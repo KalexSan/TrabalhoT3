050e5f8fa00796786c9467b7ae387c60fca1428e
Alex Sander <zicotvo991@gmail.com>
2023-07-02 20:01:30 -0300

    apenas pequenas alterações

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import { ref } from "vue";
 import axios from "axios";
 
 const imagem = ref([]);
+const query = ref("");
 
 const getImgens = async () => {
   const resposta = await axios.get("https://pixabay.com/api/?key=37298216-cbba297a2176659fc030715a4&q=yellow+flowers&image_type=photo");
@@ -10,22 +11,31 @@ const getImgens = async () => {
   imagem.value = resposta.data.hits;
   // console.log(imagem.value);
 }
+
+return {
+  imagens,
+  query,
+  getImgens
+}
 </script>
 
 <template>
-  <!-- header -->
   <div>
+    <!-- cabeçalho -->
 
-  </div>
-  <!-- a função de puxar as imagens da API e colocar um pesquisa de imagens -->
-  <main class="h-screen bg-blue-300">
-    <input class="border-2 rounded-md border-red-600" type="text"/>
+    <!-- O campo de busca de imagens -->
+    <input class="border-2 rounded-md border-red-600" type="text" v-model="query" />
+
+    <!-- O botão para acionar a busca de imagens -->
     <button class="px-4 py-2 bg-black text-white font-bold rounded-md hover:bg-gray-700" @click="getImgens">
       Clique aqui
     </button>
-    <div class="columns-3">
-      <img class="m-3 hover:scale-105 hover:duration-700" :src="img.webformatURL" v-for="img in imagem">
+
+    <!-- A grade de imagens -->
+    <div class="grid grid-cols-3 gap-3 m-3">
+      <img class="hover:scale-105 hover:duration-700" :src="img.webformatURL" v-for="img in imagens"
+        :alt="img.tags" />
     </div>
-  </main>
+  </div>
 </template>
 
