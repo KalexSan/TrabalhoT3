c4c0b937bc02377c0fc11072494ee960eb1a17c1
Alex Sander <zicotvo991@gmail.com>
2023-07-03 00:22:17 -0300

    finalizei o TRABALHO

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,59 @@
 <script setup>
-import { ref } from "vue";
+import { ref, watch } from "vue";
 import axios from "axios";
 
-const imagem = ref([]);
-const query = ref("");
+const imagens = ref([]);
+const userDig = ref("");
+const paginaAtual = ref(1);
 
 const getImgens = async () => {
-  const resposta = await axios.get("https://pixabay.com/api/?key=37298216-cbba297a2176659fc030715a4&q=yellow+flowers&image_type=photo");
+  const resposta = await axios.get('https://pixabay.com/api/?key=37298216-cbba297a2176659fc030715a4&q='+ encodeURIComponent(userDig.value) +'&image_type=photo&page=' + paginaAtual.value);
 
-  imagem.value = resposta.data.hits;
+  imagens.value = imagens.value.concat(resposta.data.hits);
+
   // console.log(imagem.value);
 }
 
-return {
-  imagens,
-  query,
-  getImgens
+const buscarMaisImagens = () => {
+  paginaAtual.value++;
+  getImgens();
 }
+
+watch(userDig, async (newValue, oldValue) => {
+    if (newValue == oldValue) {
+      return;
+    }
+    paginaAtual.value = 1;
+    imagens.value = [];
+})
+
 </script>
 
 <template>
   <div>
-    <!-- cabeçalho -->
+    <div class="flex flex-col items-center m-4">
+        <h1 class="m-4 text-2xl font-bold font-serif">
+          Busca de imagens para baixar
+        </h1>
+      <div>
+        <!-- O campo de busca de imagens -->
+        <input class="border-2 rounded-md border-red-600 m-3" type="text" v-model="userDig" />
 
-    <!-- O campo de busca de imagens -->
-    <input class="border-2 rounded-md border-red-600" type="text" v-model="query" />
-
-    <!-- O botão para acionar a busca de imagens -->
-    <button class="px-4 py-2 bg-black text-white font-bold rounded-md hover:bg-gray-700" @click="getImgens">
-      Clique aqui
-    </button>
-
+        <!-- O botão para acionar a busca de imagens -->
+        <button class="px-4 py-2 bg-black text-white font-bold rounded-md hover:bg-gray-800" @click="getImgens">
+          Clique aqui
+        </button>
+      </div>
+    </div>
     <!-- A grade de imagens -->
-    <div class="grid grid-cols-3 gap-3 m-3">
-      <img class="hover:scale-105 hover:duration-700" :src="img.webformatURL" v-for="img in imagens"
-        :alt="img.tags" />
+    <div class="columns-xs">
+      <img class="hover:scale-105 hover:duration-700 m-1" :src="img.webformatURL" v-for="img in imagens" />
+    </div>
+     <!-- Botão para buscar mais imagens -->
+     <div class="flex justify-center mt-4">
+      <button class="px-4 py-2 m-4 bg-black text-white font-bold rounded-md hover:bg-gray-800" @click="buscarMaisImagens">
+        Carregar mais imagens
+      </button>
     </div>
   </div>
 </template>
